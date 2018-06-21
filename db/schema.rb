581f3856de85e28d8373941b48c965210e0c091b
Alan Kendy Higa <alan.higa@iugu.com>
2018-06-21 18:02:49 +0000

    complete authentication system

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180608171259) do
+ActiveRecord::Schema.define(version: 20180613181405) do
 
   create_table "articles", force: :cascade do |t|
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.string   "title"
   end
 
   create_table "users", force: :cascade do |t|
