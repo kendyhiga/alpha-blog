ead5b950269d24346fb588de074b53f8a6aff0a9
Alan Kendy Higa <alan.higa@iugu.com>
2018-06-25 20:26:12 +0000

    Fix trial, third time

--- db/migrate/20180613181405_add_title_to_articles.rb
+++ db/migrate/20180613181405_add_title_to_articles.rb
@@ -1,5 +1,8 @@
 class AddTitleToArticles < ActiveRecord::Migration
   def change
+    if column_exists? :articles, :title
+      remove_column :articles, :title
+    end
     add_column :articles, :title, :string
   end
 end
