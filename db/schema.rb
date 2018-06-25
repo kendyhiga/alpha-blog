b47654317372f20352fce4d4e9d4bf68af95c651
Alan Kendy Higa <alan.higa@iugu.com>
2018-06-25 18:20:45 +0000

    Add admin user delete functionality

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180613181405) do
+ActiveRecord::Schema.define(version: 20180622155507) do
 
   create_table "articles", force: :cascade do |t|
     t.text     "description"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20180613181405) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.boolean  "admin",           default: false
   end
 
 end
